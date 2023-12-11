ed5b2195cc560bb0ef5465847d8b08216735a96f
Ava Silver <avapsilver@gmail.com>
2023-12-10 21:31:25 -0500

    9b

---
+++ 2023/day09/b.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main() {
+  final histories = File('input.txt')
+      .readAsLinesSync()
+      .map((l) => l.split(' ').map(int.parse).toList().reversed.toList());
+  final nextValues = histories.map((history) {
+    final stack = <List<int>>[history];
+    while (stack.last.any((e) => e != 0)) {
+      stack.add(List.generate(stack.last.length - 1,
+          (idx) => stack.last[idx + 1] - stack.last[idx]));
+    }
+    for (var idx = stack.length - 2; idx >= 0; idx--) {
+      stack[idx].add(stack[idx].last + stack[idx + 1].last);
+    }
+    return stack[0].last;
+  });
+
+  final sum = nextValues.reduce((a, b) => a + b);
+  print(sum);
+}
