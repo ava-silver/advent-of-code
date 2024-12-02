f9970986eec78fdcbbae36cc9353c62cd2c5b17e
Ava Silver <avapsilver@gmail.com>
2024-12-01 20:59:20 -0500

    refactor again

--- 2024/aoc01/src/main/scala/Main.scala
+++ 2024/aoc01/src/main/scala/Main.scala
@@ -6,16 +6,15 @@ def parse(input: String): (Array[Int], Array[Int]) = input
   .map(_.split("\\s+").map(_.toInt))
   .unzip(nums => (nums(0), nums(1)))
 
-def part1(input: String): Int =
-  val (list1, list2) = parse(input)
+def part1(list1: Array[Int], list2: Array[Int]): Int =
   list1.sorted.zip(list2.sorted).map { case (n1, n2) => (n1 - n2).abs }.sum
 
-def part2(input: String): Int =
-  val (list1, list2) = parse(input)
+def part2(list1: Array[Int], list2: Array[Int]): Int =
   val freq = list2.groupBy(identity).mapValues(_.size).toMap
   list1.map(n => freq.getOrElse(n, 0) * n).sum
 
 @main def main(): Unit =
   val txt = fromResource("input.txt").mkString
-  println(part1(txt))
-  println(part2(txt))
+  val (list1, list2) = parse(txt)
+  println(part1(list1, list2))
+  println(part2(list1, list2))
--- 2024/aoc01/src/test/scala/MainSuite.scala
+++ 2024/aoc01/src/test/scala/MainSuite.scala
@@ -1,18 +1,18 @@
 import munit.FunSuite
 import aoc01._
 
-def testInput = """3   4
+val (l1, l2) = parse("""3   4
 4   3
 2   5
 1   3
 3   9
-3   3"""
+3   3""")
 
 class MySuite extends FunSuite {
   test("part1") {
-    assertEquals(part1(testInput), 11)
+    assertEquals(part1(l1, l2), 11)
   }
   test("part2") {
-    assertEquals(part2(testInput), 31)
+    assertEquals(part2(l1, l2), 31)
   }
 }
