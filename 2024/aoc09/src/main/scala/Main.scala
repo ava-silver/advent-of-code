08b5efbc3e8e410660bf223ac947d317574054e8
Ava Silver <avapsilver@gmail.com>
2024-12-11 21:47:23 -0500

    day 9 pt 2

--- 2024/aoc09/src/main/scala/Main.scala
+++ 2024/aoc09/src/main/scala/Main.scala
@@ -45,8 +45,12 @@ def part1(input: String): BigInt =
   checksum(blocks)
 
 enum DiskItem:
-  case Space(size: Int, moved: Boolean)
+  case Space(size: Int)
   case File(size: Int, moved: Boolean, value: Int)
+
+def isMoved(i: DiskItem): Boolean = i match
+  case File(_, moved, _) => moved
+  case Space(_)          => true
 
 def parse2(input: String): ArrayDeque[DiskItem] =
   var idx = 0
@@ -55,13 +59,55 @@ def parse2(input: String): ArrayDeque[DiskItem] =
     .split("")
     .map { c =>
       val block =
-        if space then Space(c.toInt, false) else File(c.toInt, false, idx)
+        if space then Space(c.toInt) else File(c.toInt, false, idx)
       if !space then idx += 1
       space = !space
       block
     }
     .to(ArrayDeque)
 
+def putSpace(blocks: ArrayDeque[DiskItem], size: Int, idx: Int): Unit =
+  (blocks(idx - 1), blocks.lift(idx + 1)) match
+    case (File(_, _, _), Some(Space(spaceRSize))) =>
+      blocks.remove(idx)
+      blocks(idx) = Space(size + spaceRSize)
+    case (Space(spaceLSize), Some(Space(spaceRSize))) =>
+      blocks(idx) = Space(size + spaceLSize + spaceRSize)
+      blocks.remove(idx + 1)
+      blocks.remove(idx - 1)
+    case (Space(spaceLSize), _) =>
+      blocks(idx) = Space(size + spaceLSize)
+      blocks.remove(idx - 1)
+    case (File(_, _, _), _) =>
+      blocks(idx) = Space(size)
+
 def part2(input: String): BigInt =
   val blocks = parse2(input)
-  blocks.length
+  while (!blocks.forall(isMoved)) {
+    val fileIdx = blocks.lastIndexWhere(!isMoved(_))
+    val File(fileSize, _, value) = blocks(fileIdx): @unchecked
+    val spaceIdx = blocks.indexWhere {
+      case File(_, _, _) => false
+      case Space(size)   => fileSize <= size
+    }
+    if spaceIdx < 0 || spaceIdx > fileIdx then
+      blocks(fileIdx) = File(fileSize, true, value)
+    else {
+      val Space(spaceSize) = blocks(spaceIdx): @unchecked
+      val spaceRemaining = spaceSize - fileSize
+      blocks(spaceIdx) = File(fileSize, true, value)
+      putSpace(blocks, fileSize, fileIdx)
+      if spaceRemaining > 0 then
+        blocks.insert(spaceIdx + 1, Space(spaceRemaining))
+    }
+  }
+  var idx = 0
+  blocks.map {
+    case Space(size) => idx += size; BigInt(0)
+    case File(size, _, value) => {
+      val v = BigInt(value)
+      val sum = (idx until idx + size).map(_ * v).sum
+      idx += size
+      sum
+    }
+  }.sum
