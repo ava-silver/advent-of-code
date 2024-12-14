8638f692f3fcc9fd9dfe4789946f9b1b525e6ae7
Ava Silver <ava.silver@datadoghq.com>
2024-12-14 13:02:30 -0500

    day 10a

--- 2024/aoc10/src/main/scala/Main.scala
+++ 2024/aoc10/src/main/scala/Main.scala
@@ -5,5 +5,30 @@ package aoc10
   println(part1(input))
   println(part2(input))
 
-def part1(input: String): Int = 0
+def parse(input: String): Array[Array[Int]] =
+  input.split("\n").map(_.toCharArray.map(_.toInt))
+
+val directions = Array((0, 1), (1, 0), (-1, 0), (0, -1))
+
+def addPoints(p1: (Int, Int), p2: (Int, Int)): (Int, Int) =
+  (p1._1 + p2._1, p1._2 + p2._2)
+
+def findApexes(map: Array[Array[T]], p0: (Int, Int)): Set[(Int, Int)] =
+  val height = map(p0._2)(p0._1)
+  if height == 9 then Set(p0)
+  else
+    directions
+      .filter(p => (map(p._2)(p._1) - height) == 1)
+      .flatMap(findApexes(map, _))
+      .toSet
+
+def part1(input: String): Int =
+  val map = parse(input)
+  val trailheads = map.zipWithIndex.flatMap { case (row, y) =>
+    row.zipWithIndex.flatMap { case (height, x) =>
+      if height == 0 then Array((x, y)) else Array()
+    }
+  }
+  trailheads.flatMap(findApexes(map, _)).sum
+
 def part2(input: String): Int = 0
--- 2024/aoc10/src/test/scala/MySuite.scala
+++ 2024/aoc10/src/test/scala/MySuite.scala
@@ -2,10 +2,18 @@
 // https://scalameta.org/munit/docs/getting-started.html
 
 import aoc10._
-val input = ""
+val input = """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+"""
 class MySuite extends munit.FunSuite {
   test("part1") {
-    assertEquals(part1(input), 0)
+    assertEquals(part1(input), 36)
   }
   test("part2") {
     assertEquals(part2(input), 0)
