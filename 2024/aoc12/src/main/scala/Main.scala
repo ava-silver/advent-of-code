1973556da1d385805d6bad36adf10f41d121251f
Ava Silver <ava.silver@datadoghq.com>
2024-12-15 09:05:17 -0800

    day 12b not finished

--- 2024/aoc12/src/main/scala/Main.scala
+++ 2024/aoc12/src/main/scala/Main.scala
@@ -14,7 +14,9 @@ def parse(input: String): Array[Array[Char]] =
     .split("\n")
     .map(_.toCharArray)
 
-def groupByType(map: Array[Array[Char]]): Map[Char, Array[(Int, Int)]] =
+type Point = (Int, Int)
+
+def groupByType(map: Array[Array[Char]]): Map[Char, Array[Point]] =
   map.zipWithIndex
     .flatMap { case (row, y) =>
       row.zipWithIndex.map { case (c, x) =>
@@ -22,31 +24,32 @@ def groupByType(map: Array[Array[Char]]): Map[Char, Array[(Int, Int)]] =
       }
     }
     .foldLeft(
-      Map.empty[Char, Array[(Int, Int)]].withDefault(_ => Array[(Int, Int)]())
+      Map.empty[Char, Array[Point]].withDefault(_ => Array[Point]())
     ) { case (map, (c, p)) =>
       map.updated(c, map(c) :+ p)
     }
+
 val directions = Array((0, 1), (1, 0), (-1, 0), (0, -1))
 
-def addPoints(p1: (Int, Int), p2: (Int, Int)): (Int, Int) =
+def addPoints(p1: Point, p2: Point): Point =
   (p1._1 + p2._1, p1._2 + p2._2)
 
-def getMap(map: Array[Array[Char]], p: (Int, Int)): Option[Char] =
+def getMap(map: Array[Array[Char]], p: Point): Option[Char] =
   map.lift(p._2).flatMap(_.lift(p._1))
 
-def perimiter(map: Array[Array[Char]], points: Array[(Int, Int)]): Int =
+def perimiter(map: Array[Array[Char]], points: Array[Point]): Int =
   val c = getMap(map, points(0))
   points.map { p =>
     directions.count(d => (getMap(map, addPoints(p, d)) != c))
   }.sum
 
 def getRegions(
-    points: Array[(Int, Int)]
-): Array[Array[(Int, Int)]] =
+    points: Array[Point]
+): Array[Array[Point]] =
   val pointsRemaining = points.to(mutable.Set)
-  val regions = mutable.ArrayBuffer[Array[(Int, Int)]]()
+  val regions = mutable.ArrayBuffer[Array[Point]]()
   while (!pointsRemaining.isEmpty) {
-    var region = mutable.Set[(Int, Int)]()
+    var region = mutable.Set[Point]()
     val p = pointsRemaining.head
     pointsRemaining.remove(p)
     region.add(p)
@@ -71,4 +74,17 @@ def part1(input: String): Int =
     .map(r => perimiter(map, r) * r.length)
     .sum
 
-def part2(input: String): Int = 0
+def dot(p1: Point, p2: Point): Int =
+  (p1._1 * p2._1) + (p1._2 * p2._2)
+
+def sides(region: Array[Point]): Int =
+  // todo
+  0
+
+def part2(input: String): Int =
+  val map = parse(input)
+  val pointsByChar = groupByType(map)
+  pointsByChar.values
+    .flatMap(getRegions)
+    .map(r => sides(r) * r.length)
+    .sum
