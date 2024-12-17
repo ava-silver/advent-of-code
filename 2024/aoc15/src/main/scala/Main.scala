599d6db10d08c4c5d8436262394dd0da1b39fc1e
Ava Silver <ava.silver@datadoghq.com>
2024-12-16 21:52:25 -0800

    day 15a

--- 2024/aoc15/src/main/scala/Main.scala
+++ 2024/aoc15/src/main/scala/Main.scala
@@ -1,76 +1,80 @@
 package aoc15
 
 import scala.io.Source.fromResource
+import scala.collection.mutable
 
 @main def main(): Unit =
   val input = fromResource("input.txt").mkString
-  println(part1(input, (101, 103)))
-  part2(input)
+  println(part1(input))
+  println(part2(input))
 
 type Point = (Int, Int)
 
 implicit class PointOps(val p: Point) extends AnyVal {
   def +(other: Point): Point = (p._1 + other._1, p._2 + other._2)
   def *(other: Int): Point = (p._1 * other, p._2 * other)
-  def %(other: Point): Point =
-    ((other._1 + p._1) % other._1, (other._2 + p._2) % other._2)
 }
 
-case class Robot(position: Point, velocity: Point)
-
-def parse(input: String): Array[Robot] =
-  input.split("\n").map { line =>
-    val toks = line
-      .split(" ")
-      .map(_.split("=")(1).split(",").map(_.toInt))
-      .map(p => (p(0), p(1)))
-    Robot(toks(0), toks(1))
-  }
-
-def move(r: Robot, bounds: Point): Robot =
-  Robot((r.position + r.velocity) % bounds, r.velocity)
-
-def part1(input: String, bounds: Point): Int =
-  var robots = parse(input)
-  for (_ <- 1 to 100) {
-    robots = robots.map(move(_, bounds))
-  }
-  val (centerX, centerY) = (bounds._1 / 2, bounds._2 / 2)
-  robots.count { r =>
-    val (x, y) = r.position
-    x > centerX && y > centerY
-  } * robots.count { r =>
-    val (x, y) = r.position
-    x < centerX && y < centerY
-  } * robots.count { r =>
-    val (x, y) = r.position
-    x < centerX && y > centerY
-  } * robots.count { r =>
-    val (x, y) = r.position
-    x > centerX && y < centerY
-  }
-
-def printRobots(rs: Array[Robot], bounds: Point) =
-  println(
-    (0 until bounds._2)
-      .map { y =>
-        (0 until bounds._1).map { x =>
-          {
-            val c = rs.count(_.position == (x, y))
-            if c == 0 then "  " else "OO"
-          }
-        }.mkString
-      }
-      .mkString("\n")
-  )
-def part2(input: String): Unit =
-  var robots = parse(input)
-  val bounds = (101, 103)
-  for (i <- LazyList.from(1)) {
-    robots = robots.map(move(_, bounds))
-    if ((i - 79) % 101 == 0) then {
-      println(f"-------------- Second $i --------------")
-      printRobots(robots, bounds)
-      Thread.sleep(150)
+// returns the robot position, set of boxes (mutable) and set of walls, as well as the instructions
+def parse(
+    input: String
+): (Point, mutable.Set[Point], Set[Point], Array[Char]) =
+  val parts = input.split("\n\n")
+  val map = parts(0).split("\n").map(_.toCharArray)
+  val robotY = map.indexWhere(_.contains('@'))
+  val robotX = map(robotY).indexOf('@')
+  val boxes = mutable.HashSet[Point]()
+  val walls = map.zipWithIndex.flatMap { case (row, y) =>
+    row.zipWithIndex.flatMap { case (c, x) =>
+      if c == 'O' then boxes.add((x, y))
+      if c == '#' then Array((x, y)) else Array[Point]()
     }
+  }.toSet
+  (
+    (robotX, robotY),
+    boxes,
+    walls,
+    parts(1).replace("\n", "").toCharArray
+  )
+
+val instrToDir = Map(
+  '<' -> (-1, 0),
+  '^' -> (0, -1),
+  '>' -> (1, 0),
+  'v' -> (0, 1)
+)
+
+def move(
+    robot: Point,
+    boxes: mutable.Set[Point],
+    walls: Set[Point],
+    move: Point
+): Point =
+  def canMove(p: Point, move: Point): Boolean =
+    val newP = p + move
+    !walls.contains(newP) && (!boxes.contains(newP) || canMove(newP, move))
+
+  def moveBoxes(p: Point, move: Point): Unit =
+    val newP = p + move
+    if boxes.remove(newP) then {
+      moveBoxes(newP, move)
+      boxes.add(newP + move)
+    }
+
+  val nextPlace = robot + move
+  if canMove(robot, move) then {
+    moveBoxes(robot, move)
+    nextPlace
+  } else {
+    robot
   }
+
+def part1(input: String): Int =
+  val (robotStart, boxes, walls, instructions) = parse(input)
+  var robot = robotStart
+  for (instruction <- instructions) {
+    robot = move(robot, boxes, walls, instrToDir(instruction))
+  }
+  boxes.map { case (x, y) => x + (y * 100) }.sum
+
+def part2(input: String): Int = 0
